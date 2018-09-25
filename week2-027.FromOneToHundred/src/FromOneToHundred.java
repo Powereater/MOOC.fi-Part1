d60e40f0ba061907102420fab03bd0f1e7962da1
Powereater <37006583+Powereater@users.noreply.github.com>
2018-09-25 00:01:38 -0400

    Add files via upload

---
+++ week2-027.FromOneToHundred/src/FromOneToHundred.java
@@ -0,0 +1,17 @@
+
+public class FromOneToHundred {
+
+    public static void main(String[] args) {
+       
+        // Write your program here
+        int number = 0;
+       while (number < 100){
+           number++;
+           System.out.print(number + "\n");
+       }
+           
+                
+               
+                
+    }
+}
